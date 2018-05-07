ef1367222ed67c20ae7a463ef879b09d7409cb78
JaroslavX <jarborx@gmail.com>
2018-05-07 20:14:03 +0300

    Lesson 03 Homework

--- src/app/lessons/02/l-02.js
+++ src/app/lessons/02/l-02.js
@@ -270,12 +270,12 @@ console.log('\nTask 02.01: TODO: Review the contents of this file and make sure 
 // Task 02.01
 // 1 point
 // TODO: if you performed this task, please uncomment next line:
-// console.log('I\'ve reviewed the l-02.js file and I understand it');
+console.log('I\'ve reviewed the l-02.js file and I understand it');
 
 console.log('\nTask 02.02:');
 // Task 02.02
 // 1 point
 // Using String.substring method, log to the console characters 5-8 of the 'helloWorld' string.
 // TODO: write code here
-
+console.log('helloWorld'.substring(5,8));
 console.log('\nLesson 02 - Homework End.');
